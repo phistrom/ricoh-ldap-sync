24810102aa3d5c7c9f1121dbe1311a02b6b4f6c2
Phillip Stromberg <phillip@strombergs.com>
2019-09-20 12:40:18 -0500

    Added pypi stuff

--- ricoh_ldap_sync/__init__.py
+++ ricoh_ldap_sync/__init__.py
@@ -1,3 +1,5 @@
+__VERSION__ = "0.1.0"
+
 from .log import logger
 from .constants import *
 from .user import User, USER_CSV_FIELDS
--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
+from codecs import open
+import os
 from setuptools import setup
+from ricoh_ldap_sync import __VERSION__
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, 'README.md'), 'r') as infile:
+    long_description = infile.read()
 
 setup(
     name='ricoh-ldap-sync',
-    version='0.1',
+    version=__VERSION__,
     packages=['ricoh_ldap_sync'],
-    url='',
+    url='https://github.com/phistrom/ricoh-ldap-sync',
     license='MIT',
     author='Phillip Stromberg',
     author_email='phillip@strombergs.com',
@@ -13,9 +21,22 @@ setup(
         "pyad==0.6.0",
     ],
     zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: System Administrators',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
     entry_points={
         'console_scripts': [
             'ricoh-sync = ricoh_ldap_sync.cli:command_line',
         ],
     },
+    long_description=long_description,
+    long_description_content_type="text/markdown"
 )
